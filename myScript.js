e1b0e864d1f0f1e01c694b7463c2bbb97c1d7c8b
Ashkon Aghassi <aaghassi2018@francisparker.org>
2016-10-31 23:36:33 -0700

    the javascript in this thing is pretty sick i do have to say

---
+++ myScript.js
@@ -0,0 +1,10 @@
+function changeColor(){
+  if(document.getElementById('testing').style.backgroundColor=='yellow'){
+    document.getElementById('testing').style.backgroundColor='lightgrey';
+  }
+  else {
+    {
+      document.getElementById('testing').style.backgroundColor='yellow';
+    }
+  }
+}
