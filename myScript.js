6bf5f6fd92072a62dbfb915c1f5dc785e461b3a8
Ashkon Aghassi <aaghassi2018@francisparker.org>
2016-11-02 13:59:41 -0700

    no message

--- myScript.js
+++ myScript.js
@@ -1,10 +1,13 @@
 function changeColor(color){
   if(color=='lightgrey'){
     document.getElementById('testing').style.backgroundColor='lightgrey';
+    document.getElementById('blahblah').style.backgroundColor='darkgrey';
   }
   else {
     {
       document.getElementById('testing').style.backgroundColor='yellow';
+      document.getElementById('blahblah').style.backgroundColor='black';
+
     }
   }
 }
